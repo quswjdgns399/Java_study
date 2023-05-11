690c246f47f5db4395ffa72baae8078184b30e8f
quswjdgns399 <quswjdgns3996@gmail.com>
2023-05-11 16:31:29 +0900

    First Commit

---
+++ src/main/java/beakjun/beakjun_2751.java
@@ -0,0 +1,41 @@
+package beakjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class beakjun_2751 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));*/
+
+       /* Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());*/
+
+        int num = sc.nextInt();
+
+        // list 계열 중 하나를 쓰면 된다.
+        ArrayList<Integer> list = new ArrayList<>();
+
+       for(int i=0; i<num; i++){
+           list.add(sc.nextInt());
+       }
+
+        Collections.sort(list);
+
+        for(int value : list) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+
+
+
+    }
+
+}
---
+++ src/main/java/beakjun/bubble_Buffer.java
@@ -0,0 +1,33 @@
+package beakjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bubble_Buffer {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+
+        for(int i=0; i<num; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i=0; i<num-1; i++){
+            for(int j=i+1; j<num; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i<num; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
