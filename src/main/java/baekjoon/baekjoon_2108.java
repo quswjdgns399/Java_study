260dbb9ddfdef7e0e22e113646e516db67fa0506
quswjdgns399 <quswjdgns3996@gmail.com>
2023-05-23 17:35:13 +0900

    First Commit

--- src/main/java/baekjoon/baekjoon_2108.java
+++ src/main/java/baekjoon/baekjoon_2108.java
@@ -1,63 +1,77 @@
-//시간초과 실패 ㅠㅠ
-
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class baekjoon_2108 {
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        int[] arr = new int[num];
-        int[] sorted_arr = new int[num];
+    public static int num;
+    public static int[] array;
 
-        double sum = 0;
-        for(int i=0; i<num; i++){
-            arr[i] = sorted_arr[i] = Integer.parseInt(br.readLine());
-            sum = sum + arr[i];
+    public static int getAverage() {
+        // 배열을 다 합해주는 stream
+        double sum = Arrays.stream(array).sum();
+
+        double avg = (double) sum / num;
+        if (avg < 0) {
+            // 반올림하는 Math.round함수
+            return (int) Math.round(Math.abs(avg) * -1);
+        } else {
+            return (int) Math.round(avg);
         }
+    }
 
-        Arrays.sort(sorted_arr);
+    public static int countFreq() {
+        Map<Integer, Integer> mp = new HashMap<>();
 
-        int avg = (int)Math.round(sum / num);
-
-        int mid = sorted_arr[num/2];
-
-        int range = sorted_arr[num-1] - sorted_arr[0];
-
-        //최빈값 시작
-        int rank=0;
-
-        if(num==1) {
-            hashMap.put(rank, arr[0]);
+        if (num == 1) {
+            return array[0];
         }
-        else {
-            for (int i = 0; i < num - 1; i++) {
-                for (int j = i + 1; j < num; j++) {
-                    if (Arrays.asList(sorted_arr[j]).contains(sorted_arr[i])) {
-                        hashMap.put(rank, sorted_arr[i]);
-                    }
-                    else
-                        hashMap.put(rank, sorted_arr[1]);
-                }
+        for (int i = 0; i < num; i++) {
+            if (mp.containsKey(array[i])) {
+                mp.put(array[i], mp.get(array[i]) + 1);
+            } else {
+                mp.put(array[i], 1);
             }
         }
+        int maxValue = Collections.max(mp.values());
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        // 가장 많이 나온 값
+        for (Map.Entry<Integer, Integer> m : mp.entrySet()) {
+            if (m.getValue() == maxValue) {
+                arrayList.add(m.getKey());
+            }
+        }
+        Collections.sort(arrayList);
+        // 가장 많이 나온 값이 여러개일 경우 두번째로 작은 값
+        if (arrayList.size() > 1)
+            return arrayList.get(1);
+        else // 가장 많이 나온 값이 하나면
+            return arrayList.get(0);
+    }
 
-        sb.append(avg).append('\n');
-        sb.append(mid).append('\n');
-        sb.append(hashMap.get(0)).append('\n');
-        sb.append(range).append('\n');
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
 
-        System.out.println(sb);
-
+        array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = scan.nextInt();
+        }
+        // 산술평균
+        System.out.println(getAverage());
+        Arrays.sort(array);
+        // 중앙값
+        System.out.println(array[num / 2]);
+        // 최빈값
+        System.out.println(countFreq());
+        // 범위
+        System.out.println(array[num - 1] - array[0]);
+        scan.close();
     }
 }
